330df683a5293c69e3e6dd9eeb35dc8458f29420
Miguel™ <Miguel>
2016-09-11 20:17:01 -0500

    Terminado

--- PokemonApp/app/src/main/java/pe/edu/ulima/pokemonapp/PreguntaActivity.java
+++ PokemonApp/app/src/main/java/pe/edu/ulima/pokemonapp/PreguntaActivity.java
@@ -1,13 +1,81 @@
 package pe.edu.ulima.pokemonapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import pe.edu.ulima.pokemonapp.model.GestorPokemonApp;
+import pe.edu.ulima.pokemonapp.model.Pregunta;
 
 public class PreguntaActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView1;
+    TextView textView2;
+    EditText editText;
+    ImageButton imageButton1;
+    ImageButton imageButton2;
+    List<Pregunta> lista = GestorPokemonApp.getInstance().obtenerPreguntas();
+    int i=0;
+    private int puntaje=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(savedInstanceState!=null){
+            i= savedInstanceState.getInt("posicion");
+            puntaje= savedInstanceState.getInt("puntaje");
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pregunta);
+        Bundle bundle= getIntent().getExtras();
+        String user= bundle.getString("user");
+        imageView= (ImageView) findViewById(R.id.imgPok);
+        textView1= (TextView) findViewById(R.id.txtNumero);
+        textView2= (TextView) findViewById(R.id.txtPregunta);
+        editText= (EditText) findViewById(R.id.edtRespuesta);
+        imageButton1= (ImageButton) findViewById(R.id.butOk);
+        imageButton2= (ImageButton) findViewById(R.id.butNext);
+        textView1.setText(String.format("Pregunta %s", lista.get(i).getId()));
+        textView2.setText(lista.get(i).getPregunta());
+
+    }
+    public void Validar(View v){
+            if(editText.getText().toString().equalsIgnoreCase(lista.get(i).getRespuesta())){
+                imageView.setImageResource(lista.get(i).getImagen());
+                Toast toast = Toast.makeText(this, "Respuesta correcta", Toast.LENGTH_SHORT);
+                toast.show();
+                puntaje+=10;
+            }else{
+                Toast toast1=Toast.makeText(this, "Respuesta incorrecta", Toast.LENGTH_SHORT);
+                toast1.show();
+            }
+    }
+    public void Siguiente(View v){
+        i++;
+        if(i==lista.size()){
+            Intent intent= new Intent(this, PuntajeActivity.class);
+            intent.putExtra("puntos", puntaje);
+            startActivity(intent);
+        }else{
+            textView1.setText(String.format("Pregunta %s", lista.get(i).getId()));
+            textView2.setText(lista.get(i).getPregunta());
+            imageView.setImageResource(R.drawable.pokeball);
+            editText.setText("");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("posicion", i);
+        savedInstanceState.putInt("puntaje", puntaje);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
--- PokemonApp/app/src/main/java/pe/edu/ulima/pokemonapp/PuntajeActivity.java
+++ PokemonApp/app/src/main/java/pe/edu/ulima/pokemonapp/PuntajeActivity.java
@@ -2,6 +2,7 @@ package pe.edu.ulima.pokemonapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PuntajeActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class PuntajeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntaje);
+        TextView textView= (TextView) findViewById(R.id.txtPuntaje);
+        Bundle bundle= getIntent().getExtras();
+        int puntaje= bundle.getInt("puntos");
+        textView.setText(String.valueOf(puntaje));
     }
 }
