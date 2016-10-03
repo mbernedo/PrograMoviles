1e9fb4cea0e57e4f37a7ea7a2641635d556958aa
Miguel™ <Miguel>
2016-10-03 00:50:47 -0500

    Pc2 casi

---
+++ app/src/main/java/com/example/migue_000/pc2_20101368/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.migue_000.pc2_20101368;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText user;
+    EditText pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        user = (EditText) findViewById(R.id.eteUser);
+        pass = (EditText) findViewById(R.id.etePass);
+
+    }
+    public void Ingresar(View v){
+        Intent intent = new Intent(this,RegistroActivity.class);
+    }
+    public void Registrar(View v){
+        Intent intent2 = new Intent(this, RegistroActivity.class);
+        startActivity(intent2);
+    }
+}
---
+++ app/src/main/java/com/example/migue_000/pc2_20101368/RegistroActivity.java
@@ -0,0 +1,13 @@
+package com.example.migue_000.pc2_20101368;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
